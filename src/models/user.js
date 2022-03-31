728c286feb6bcfc8e3d3660d26b44c06d5cc0109
toanpqph14805 <toanpqph14805@fpt.edu.vn>
2022-03-31 12:06:28 +0700

    feat: user

--- src/models/user.js
+++ src/models/user.js
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from 'uuid'
 // const { ObjectId } = mongoose.Types;
 
 const userSchema = new Schema({
-    nickname: {
+    name: {
         type: String,
         minLength: 5,
     },
