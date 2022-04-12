747cfaa94174c2122d9123626c5d84359edc6a82
toanpqph14805 <toanpqph14805@fpt.edu.vn>
2022-04-12 14:58:26 +0700

    feat: check auth

--- src/models/cart.js
+++ src/models/cart.js
@@ -6,17 +6,24 @@ const cartSchema = new Schema({
         type: String,
         required: true
     },
-    price: {
-        type: Number,
-        required: true
-    },
-    quantity: {
-        type: Number,
-    },
     user: {
         type: ObjectId,
         ref: "User"
-    }
+    },
+    items: [{
+        item: {
+            type: ObjectId,
+            ref: "Product"
+        },
+        quantity: {
+            type: Number,
+            default: 1
+        },
+        price: {
+            type: Number,
+            default: 0
+        }
+    }]
 }, { timestamps: true})
 
 export default mongoose.model("Cart", cartSchema);
--- src/routes/product.js
+++ src/routes/product.js
@@ -2,13 +2,16 @@ import { Router } from "express";
 import {
     create, get, list, remove, update,
 } from "../controllers/product";
+import { userById } from "../controllers/user";
+import { checkAuth, isAuth, requiredSigin } from "../middlewares/checkAuth";
 
 const router = Router();
 
 router.get("/products", list);
-router.post("/products", create);
-router.get("/products/:id", get);
-router.delete("/products/:id", remove);
-router.put("/products/:id", update);
+router.post("/products/:useId", requiredSigin, create);
+router.get("/products/:id", checkAuth, get);
+router.delete("/products/:id", checkAuth, remove);
+router.put("/products/:id", checkAuth, update);
 
+router.param('userId', userById);
 export default router;
